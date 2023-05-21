33d8e9500098d546f1bc35e22492545c25584199
Mr. Lin <cufoon@qq.com>
2023-05-21 17:03:53 +0800

    feat: 修改 404 页面

--- src/pages/forbidden/index.tsx
+++ src/pages/forbidden/index.tsx
@@ -8,14 +8,14 @@ const ForbiddenPage: React.FC = () => {
   useEffect(() => {
     setTimeout(() => {
       navigate('/');
-    }, 1500);
+    }, 2000);
   }, []);
 
   return (
     <div className={styles.outer}>
       <div className={styles.png}></div>
       <div className={styles.text}>你来到了不存在的页面</div>
-      <div className={styles.hint}>1.5秒后回到首页</div>
+      <div className={styles.hint}>2秒后回到首页</div>
     </div>
   );
 };
